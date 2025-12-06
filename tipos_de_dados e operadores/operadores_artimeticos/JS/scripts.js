af1c041967a9dd259db2fada5497a4ba9ba37495
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-06 13:21:29 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/operadores_artimeticos/JS/scripts.js
@@ -0,0 +1,16 @@
+//operadores artimeticos
+//soma
+console.log(5+4);
+console.log(3+2);
+//substração
+console.log(4-3);
+console.log(3-5);
+//divisao
+console.log(4/2);
+console.log(6/3);
+//multiplicação
+console.log(2*5);
+console.log(3*7);
+//varias operações
+console.log((3+4)*4);
+console.log((3-2)*(3+4));
