3f8e48f3cab5b3c865ecba83249126cd725b73ee
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-06 12:44:18 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/number/JS/scripts.js
@@ -0,0 +1,13 @@
+//number
+//mostrando um numero
+console.log(5);
+//tanto valores quebrados quanto negativos sao considerados number
+//não se separa em int e float
+console.log(4.5);
+console.log(-4);
+
+
+//typeof mostra o tipo de dado
+console.log(typeof 5);
+console.log(typeof 4.5);
+console.log(typeof -4);
