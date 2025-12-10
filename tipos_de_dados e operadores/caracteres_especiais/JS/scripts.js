1403ef961e4968b0ff36a1411b171baf9b7b4bab
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-09 21:25:03 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/caracteres_especiais/JS/scripts.js
@@ -0,0 +1,4 @@
+//caractere especias com o \n para quebara a linha
+console.log("meu nome e samuel felipe \n eu tenho 20 anos");
+//caso queira um espaçamento de tab ultilize o \t
+console.log("meu nome e \t fernando");
