81337112c0ce2f000acd4d2bcefa5ca707559f7d
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-09 21:05:32 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/string/JS/scripts.js
@@ -0,0 +1,9 @@
+//string e um texto
+
+console.log("Samuel");
+console.log('Allan');
+console.log('5');
+//tipo de dados
+console.log(typeof "Samuel");
+console.log(typeof 'Allan');
+console.log(typeof '5');
