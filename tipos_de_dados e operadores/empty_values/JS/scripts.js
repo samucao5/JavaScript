cd3f23a3dae1e30dcab904462a62fff8f7e34fe6
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-11 00:16:06 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/empty_values/JS/scripts.js
@@ -0,0 +1,14 @@
+//Empty values
+//null e quando não ha um valor
+console.log(typeof null);
+//undefined e quando um codigo ainda não foi definido 
+console.log(typeof undefined);
+
+//ele não e identico 
+console.log(null === undefined);
+//mas sao iguais
+console.log(null == undefined);
+
+//eles não sao falsos
+console.log(null == false);
+console.log(undefined == false);
