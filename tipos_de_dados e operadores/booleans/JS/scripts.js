0539a27c2fa99002c24d3c1f59284ac7cadf6764
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-10 23:14:35 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/booleans/JS/scripts.js
@@ -0,0 +1,12 @@
+//booleans sao dois valores true ou false
+//valor verdadeiro
+console.log(true);
+console.log(30 > 5);
+
+//valor false
+console.log(false);
+console.log(30 < 5);
+
+//tipo de dado
+console.log(typeof true);
+console.log(typeof false);
