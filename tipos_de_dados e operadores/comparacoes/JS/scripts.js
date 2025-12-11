3a601202352560fc4fa135518d5afdf2992b24ba
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-10 23:41:48 -0300

    Add files via upload

---
+++ tipos_de_dados e operadores/comparacoes/JS/scripts.js
@@ -0,0 +1,18 @@
+//comparações serve de parametros para comparaçoes 
+
+//maior que >
+console.log(10 > 5);
+//menor que <
+console.log(5 < 10);
+//maior ou igual >=
+console.log(10 >= 10);
+//menor ou igual <=
+console.log(10 <= 10);
+//igual ==
+console.log(10 == 10);
+//diferente != 
+console.log(20 != 5);
+//identico ===
+console.log(10 === 10.00);
+//identico com tipo diferente !==
+console.log(10 !== "10");
