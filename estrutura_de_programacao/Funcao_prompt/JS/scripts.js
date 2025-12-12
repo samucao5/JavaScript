6fcac7f61e9f1295af1bb9e4b2c1bc10df0a3ab0
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-11 22:31:08 -0300

    Add files via upload

---
+++ estrutura_de_programacao/Funcao_prompt/JS/scripts.js
@@ -0,0 +1,5 @@
+//prompt tem como funcao receber o dado
+let nome = prompt("digite seu nome:");
+const idade = prompt("digite sua idade:");
+
+console.log(`seu nome e: ${nome}`, `voce possui ${idade} anos`);
