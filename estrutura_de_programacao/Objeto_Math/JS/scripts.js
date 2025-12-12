012450cb397768361cd15d07ddcb0d2e8c714c22
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-11 22:47:17 -0300

    Add files via upload

---
+++ estrutura_de_programacao/Objeto_Math/JS/scripts.js
@@ -0,0 +1,13 @@
+//Math é um objeto, que possui diversas funções para fins matematicos
+let num1 = prompt("digite o num1:");
+let num2 = prompt("digite o num2:");
+let num3 = prompt("digite o num3:");
+let num4 = prompt("digite o num4:");
+//esta função Math.max retorna o maior valor entre todos
+console.log(Math.max(num1,num2,num3,num4));
+//ja a função Math.min retorna o menor numero
+console.log(Math.min(num1,num2,num3,num4));
+//Math.floor arrendonda o numero para baixo
+console.log(Math.floor(5.63));
+//Math.ceil arredonda o numero para cima
+console.log(Math.ceil(5.63));
