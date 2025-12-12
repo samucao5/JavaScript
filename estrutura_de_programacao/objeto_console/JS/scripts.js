940bbe25ea7e8b569b258dc4dd9f68d391769b04
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-11 22:55:30 -0300

    Add files via upload

---
+++ estrutura_de_programacao/objeto_console/JS/scripts.js
@@ -0,0 +1,7 @@
+//console principal funcao e exibir uma mensagem de alguma categoria
+//console.log manda uma mensagem para o console
+console.log("meu nome e samuel");
+//manda uma mensagem de erro pro console
+console.error("deu errado");
+//manda uma mensagem de aviso pro console
+console.warn("pode dar errado");
