dcdfff8cd98b389904fcc0704db79129014a3289
Samuel Felipe <samuel.felipe.vr.2014@gmail.com>
2025-12-19 23:17:26 -0300

    Add files via upload

--- arrays_e_Objetos/array/JS/scripts.js
+++ arrays_e_Objetos/array/JS/scripts.js
@@ -5,3 +5,21 @@ console.log(typeof numeros);
 
 const itens = ["samuel", 20, true, 5.55, numeros];
 console.log(itens);
+//para acessar o array
+console.log(numeros[0], numeros[2]);
+
+
+//propriedade são informações de um objeto
+console.log(numeros.length);
+
+console.log(numeros["length"]);
+
+let nome = "samuel";
+
+console.log(nome.length);
+
+if(nome.length >= 6){
+    console.log("nome longo");
+}else{
+    console.log("nome curto");
+}
